6cc81ee82d5c36b0ff185830b75bc8f8e56147ae
Edward O'Dell <ed@epocreative.com>
2016-08-25 19:51:41 -0400

    first commit

---
+++ js/nav.js
@@ -0,0 +1,20 @@
+// Set up the nav scrolling effect
+$("nav").onePageNav();
+
+var menu = $("nav ul")
+
+$("#menu-icon").click(function() {
+	// Toggle the menu
+	menu.slideToggle();
+});
+
+$(window).resize(function() {
+	var width = $(window).width();
+
+	if(width > 768 && menu.is(":hidden")) {
+		menu.removeAttr("style");
+	}
+});
+
+// Add parallax
+$.stellar();
